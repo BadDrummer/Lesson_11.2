2d9e55b2cb871da6a030b0b161825e7e194c74ad
Максим <mr.maks31@mail.ru>
2023-04-16 13:55:33 +0300

    added new function "upper_first_letter", added docstrings to functions

--- str_func.py
+++ str_func.py
@@ -1,4 +1,16 @@
-'''функция данные со входа переводит в верхний регистр'''
+'''функция, которая переводит все символы строки
+со ввода в верхний регистр'''
 def upper_index(string):
     return str(string).upper()
+
+'''функция, которая делает заглавными первые буквы слов в строке,
+поступившей со ввода'''
+def upper_first_letter(string):
+    splitted_string = string.split(' ')
+
+    new_list = []
+    for word in splitted_string:
+        new_word = (word[0].upper()+word[1:])
+        new_list.append(new_word)
+    return ' '.join(new_list)
 
